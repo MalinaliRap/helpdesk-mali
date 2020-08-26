c7dd9b70ea972727c86c7fb65c5c9e919f10a51a
Gabriel Coplas Becher <gabriel.becher@cescage.edu.br>
2020-08-26 11:48:54 -0300

    primeiro commit

---
+++ helpdesk-mali-api/routes/api/v1/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('/user')->group( function () {
+    Route::get('/login', 'api\v1\PassportLoginController@login')->name('login');;
+    Route::post('/register', 'api\v1\PassportLoginController@register');
+
+    Route::middleware('auth:api')->group(function () {
+        Route::get('logout', 'api\v1\PassportLoginController@logout');
+    });
+
+});
